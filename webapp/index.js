49d9574477c0846bdaa6cf0b1f791e1f0066f8ca
yongjae cho <whdydwo2@gmail.com>
2024-12-25 10:44:22 +0000

    XML Views

--- webapp/index.js
+++ webapp/index.js
@@ -2,12 +2,23 @@
 //     "use strict";
 //     alert("UI5 is ready");
 // })
-sap.ui.define([
-    "sap/m/Text"
-], (Text) => {
-    "use strict";
 
-    new Text({
-        text: "Hello World"
-    }).placeAt("content");
+// sap.ui.define([
+//     "sap/m/Text"
+// ], (Text) => {
+//     "use strict";
+
+//     new Text({
+//         text: "Hello World"
+//     }).placeAt("content");
+// });
+
+sap.ui.define([
+    "sap/ui/core/mvc/XMLView"
+    // XMLnamespace : sap.ui.core.mvc 사용
+], (XMLView) => {
+    "use restrict";
+    XMLView.create({
+        viewName: "ui5.walkthrough.view.App"
+    }).then((oView) => oView.placeAt("content"));
 });
